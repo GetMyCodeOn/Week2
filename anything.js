74801a0de8d67d813d883efd74b3658066f7245e
Jack Pitcher <jackpitcher22@gmail.com>
2014-06-25 12:53:41 +0000

    egit

--- anything.js
+++ anything.js
@@ -1,29 +1,33 @@
   $.ajax({
-    url: "http://content.guardianapis.com/search?section=news&show-fields=trailText",
+    url: "http://content.guardianapis.com/search?q=news&show-fields=all",
     dataType: "jsonp",
     success: function(data) {
       $.each(data.response.results, function () {
         $( "#news" ).append('<a href="' + this['webUrl'] + '"><h5>' + this['webTitle'] + '<h5></a>');
+        $( "#news" ).append('<img src="' + this['fields'].thumbnail + '" />');
         $( "#news" ).append("<ul>" + this['fields'].trailText + "</ul>");
+
       });
     }});
 
   $.ajax({
-    url: "http://content.guardianapis.com/search?section=football&show-fields=trailText",
+    url: "http://content.guardianapis.com/search?section=football&show-fields=all",
     dataType: "jsonp",
     success: function(data) {
       $.each(data.response.results, function () {
         $( "#football" ).append('<a href="' + this['webUrl'] + '"><h5>' + this['webTitle'] + '<h5></a>');
+        $( "#football" ).append('<img src="' + this['fields'].thumbnail + '" />');
         $( "#football" ).append("<ul>" + this['fields'].trailText + "</ul>");
       });
     }});
 
   $.ajax({
-    url: "http://content.guardianapis.com/search?section=travel&show-fields=trailText",
+    url: "http://content.guardianapis.com/search?section=travel&show-fields=all",
     dataType: "jsonp",
     success: function(data) {
       $.each(data.response.results, function () {
         $( "#travel" ).append('<a href="' + this['webUrl'] + '"><h5>' + this['webTitle'] + '<h5></a>');
+        $( "#travel" ).append('<img src="' + this['fields'].thumbnail + '" />');
         $( "#travel" ).append("<ul>" + this['fields'].trailText + "</ul>");
       });
     }});
