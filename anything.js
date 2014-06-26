dd5479f4e0b699dfb94c15b78942c1180d994b07
Jack Pitcher <jackpitcher22@gmail.com>
2014-06-26 08:44:43 +0000

    edits

--- anything.js
+++ anything.js
@@ -1,9 +1,9 @@
   $.ajax({
-    url: "http://content.guardianapis.com/search?q=uknews&page-size=5&show-fields=trailText%2Cthumbnail",
+    url: "http://content.guardianapis.com/search?q=uknews&page-size=5&show-fields=thumbnail%2CtrailText%2CshortUrl",
     dataType: "jsonp",
     success: function(data) {
       $.each(data.response.results, function () {
-        $( "#news" ).append('<a href="' + this['webUrl'] + '"><li>' + this['webTitle'] + '</li></a>');
+        $( "#news" ).append('<a href="' + this['fields'].shortUrl + '"><li>' + this['webTitle'] + '</li></a>');
         $( "#news" ).append('<img src="' + this['fields'].thumbnail + '" />');
         $( "#news" ).append("<ul>" + this['fields'].trailText + "</ul>");
 
@@ -11,22 +11,22 @@
     }});
 
   $.ajax({
-    url: "http://content.guardianapis.com/search?q=football&page-size=5&show-fields=trailText%2Cthumbnail",
+    url: "http://content.guardianapis.com/search?q=football&page-size=5&show-fields=thumbnail%2CtrailText%2CshortUrl",
     dataType: "jsonp",
     success: function(data) {
       $.each(data.response.results, function () {
-        $( "#football" ).append('<a href="' + this['webUrl'] + '"><li>' + this['webTitle'] + '</li></a>');
+        $( "#football" ).append('<a href="' + this['fields'].shortUrl + '"><li>' + this['webTitle'] + '</li></a>');
         $( "#football" ).append('<img src="' + this['fields'].thumbnail + '" />');
         $( "#football" ).append("<ul>" + this['fields'].trailText + "</ul>");
       });
     }});
 
   $.ajax({
-    url: "http://content.guardianapis.com/search?q=travel&page-size=5&show-fields=trailText%2Cthumbnail",
+    url: "http://content.guardianapis.com/search?q=travel&page-size=5&show-fields=thumbnail%2CtrailText%2CshortUrl",
     dataType: "jsonp",
     success: function(data) {
       $.each(data.response.results, function () {
-        $( "#travel" ).append('<a href="' + this['webUrl'] + '"><li>' + this['webTitle'] + '</li></a>');
+        $( "#travel" ).append('<a href="' + this['fields'].shortUrl + '"><li>' + this['webTitle'] + '</li></a>');
         $( "#travel" ).append('<img src="' + this['fields'].thumbnail + '" />');
         $( "#travel" ).append("<ul>" + this['fields'].trailText + "</ul>");
       });
