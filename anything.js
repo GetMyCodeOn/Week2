6a8126ec5dd7322c38431a39667e0ee2dff77285
Jack Pitcher <jackpitcher22@gmail.com>
2014-06-26 12:30:58 +0000

    edit

--- anything.js
+++ anything.js
@@ -1,33 +1,24 @@
-  $.ajax({
-    url: "http://content.guardianapis.com/search?q=uknews&page-size=5&show-fields=thumbnail%2CtrailText%2CshortUrl",
-    dataType: "jsonp",
-    success: function(data) {
-      $.each(data.response.results, function () {
-        $( "#news" ).append("<br clear=all>" + '<a href="' + this['fields'].shortUrl + '"><li>' + this['webTitle'] + '</li></a>' + "</br>");
-        $( "#news" ).append('<img src="' + this['fields'].thumbnail + '" />');
-        $( "#news" ).append("<ul>" + this['fields'].trailText + "</ul>");
+var topics = {'1':"news",'2':"football",'3':"travel"};
+  
 
-      });
-    }});
 
-  $.ajax({
-    url: "http://content.guardianapis.com/search?q=football&page-size=5&show-fields=thumbnail%2CtrailText%2CshortUrl",
-    dataType: "jsonp",
-    success: function(data) {
-      $.each(data.response.results, function () {
-        $( "#football" ).append("<br clear=all>" + '<a href="' + this['fields'].shortUrl + '"><li>' + this['webTitle'] + '</li></a>' + "</br>");
-        $( "#football" ).append('<img src="' + this['fields'].thumbnail + '" />');
-        $( "#football" ).append("<ul>" + this['fields'].trailText + "</ul>");
-      });
-    }});
+  $ .each( topics, function( i, val ){
 
-  $.ajax({
-    url: "http://content.guardianapis.com/search?q=travel&page-size=5&show-fields=thumbnail%2CtrailText%2CshortUrl",
-    dataType: "jsonp",
-    success: function(data) {
-      $.each(data.response.results, function () {
-        $( "#travel" ).append("<br clear=all>" + '<a href="' + this['fields'].shortUrl + '"><li>' + this['webTitle'] + '</li></a>' + "</br>");
-        $( "#travel" ).append('<img src="' + this['fields'].thumbnail + '" />');
-        $( "#travel" ).append("<ul>" + this['fields'].trailText + "</ul>");
-      });
-    }});
+    
+
+    $.ajax({
+
+      url: "http://content.guardianapis.com/search?q="+ val +"&page-size=5&show-fields=thumbnail%2CtrailText%2CshortUrl",
+
+
+      dataType: "jsonp",
+
+      success: function(data) {
+        $.each(data.response.results, function () {
+                 $( "#" + val ).append("<br clear=all>" + '<a href="' + this['fields'].shortUrl + '"><li>' + this['webTitle'] + '</li></a>' + "</br>");
+                 $( "#" + val ).append('<a href="' + this['fields'].shortUrl + '"><img src="' + this['fields'].thumbnail + '" /></a>');
+                 $( "#" + val ).append("<ul>" + this['fields'].trailText + "</ul>");
+        });
+
+      }});
+  });
